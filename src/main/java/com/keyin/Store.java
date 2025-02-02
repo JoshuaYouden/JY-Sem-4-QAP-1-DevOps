d0ece5ce7b9b22a2372d4f55f06c42fb9318ebb5
Joshua Youden <Joshua.Youden@keyin.com>
2025-02-02 18:00:02 -0330

    Store class finished

--- src/main/java/com/keyin/Store.java
+++ src/main/java/com/keyin/Store.java
@@ -1,5 +1,29 @@
 package com.keyin;
 
-public class Store {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Store {
+    private List<Games> listOfGames;
+
+    public Store() {
+        listOfGames = new ArrayList<>();
+    }
+
+    public void addGame(Games game) {
+        listOfGames.add(game);
+    }
+
+    public Games searchGame(String title) {
+        return listOfGames.stream().filter(game -> game.getTitle().equalsIgnoreCase(title)).findFirst().orElse(null);
+    }
+
+    public void showListOfGames() {
+        if (listOfGames.isEmpty()) {
+            System.out.println("The store is empty.");
+        } else {
+            System.out.println("Current list of games:");
+            listOfGames.forEach(System.out::println);
+        }
+    }
 }
