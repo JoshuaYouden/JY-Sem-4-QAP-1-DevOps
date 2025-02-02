6265b2aa58489ea6b451f0dc72226f2d399a2071
Joshua Youden <Joshua.Youden@keyin.com>
2025-02-02 17:46:42 -0330

    Finished User.java class

--- src/main/java/com/keyin/User.java
+++ src/main/java/com/keyin/User.java
@@ -1,5 +1,35 @@
 package com.keyin;
 
-public class User {
+import java.util.ArrayList;
+import java.util.List;
 
+public class User {
+    private String username;
+    private List<Games> purchaseHistory;
+
+    public User(String username) {
+        this.username = username;
+        this.purchaseHistory = new ArrayList<>();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public List<Games> getPurchaseHistory() {
+        return purchaseHistory;
+    }
+
+    public void addPurchase(Games game) {
+        purchaseHistory.add(game);
+    }
+
+    public void purchaseHistory() {
+        if (purchaseHistory.isEmpty()) {
+            System.out.println("Purchase history is empty.");
+        } else {
+            System.out.println("Purchase history for " + username + ":");
+            purchaseHistory.forEach(System.out::println);
+        }
+    }
 }
